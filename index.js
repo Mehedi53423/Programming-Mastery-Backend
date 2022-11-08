10e6bc3c782f5ab46127d56de8222b52a71e8016
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-11-08 09:15:36 +0600

    Categories Data Added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
 
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
 
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
 app.listen(port, () => {
-  console.log("Server is Running on port ", port);
+  console.log("Server is Running on Port ", port);
 });
